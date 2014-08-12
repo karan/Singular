7c8c8dd2d464f97bb8fbe4be980a0a4ee807e556
Karan Goel <karan@goel.im>
2014-08-11 22:19:40 -0700

    start styling the app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,15 @@
+var data = { tweet_id: '499058870862741504',
+tweeted_by: 'ooiamandiinha',
+tweeted_by_photo: 'http://pbs.twimg.com/profile_images/463508828827705344/DV7FTlNk_normal.jpeg',
+song_url: 'https://www.youtube.com/watch?v=oet8ecpka_s',
+coordinates: [ -51.0952004, -29.9187868 ],
+song_source: 'yt',
+artwork_url: 'http://i.ytimg.com/vi/Oet8eCpKA_s/hqdefault.jpg',
+song_id: 'Oet8eCpKA_s',
+song_title: 'Armandinho-Ursinho de dormir' };
+
+var map;
+
 $(document).ready(function() {
 
   var mapOptions;
@@ -10,17 +22,21 @@ $(document).ready(function() {
     disableDefaultUI: true
   };
 
-  return map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-});
-
-var socket = io.connect('http://localhost:8888');
-
-socket.on('newTweet', function (data) {
   console.log(data);
   addToMap(data);
   addToSidebar(data);
+
 });
+
+// var socket = io.connect('http://localhost:8888');
+
+// socket.on('newTweet', function (data) {
+//   console.log(data);
+//   addToMap(data);
+//   addToSidebar(data);
+// });
 
 
 var addToMap = function(data) {
@@ -43,12 +59,19 @@ var addToMap = function(data) {
 }
 
 var addToSidebar = function(data) {
+  console.log("adding to sidebar");
+  var userImg = '<img src="' + data.tweeted_by_photo + '" class="profilePic">';
+  var username = '<a href="https://twitter.com/' + data.tweeted_by + '" class="username">' + data.tweeted_by + '</a>';
+  var playerCode;
   if (data.song_source === 'sc') {
-    $('#allsongs').prepend('<iframe id="sc-player" width="100%" height="100%" scrolling="no" frameborder="no", src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+song_id+'&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true"></iframe>')
+    playerCode = '<iframe id="sc-player" width="80%" height="100%" scrolling="no" frameborder="no", src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+song_id+'&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true"></iframe>';
   } else {
-    var playerCode = '<iframe id="ytplayer" type="text/html" width="100%" src="http://www.youtube.com/embed/'+data.song_id+'?autoplay=0" frameborder="0"/>';
-    $('#allsongs').prepend(playerCode);
+    playerCode = '<iframe id="ytplayer" type="text/html" width="80%" src="http://www.youtube.com/embed/'+data.song_id+'?autoplay=0" frameborder="0"/>';
   }
+  console.log(playerCode);
+  var html = '<div class="row"><div class="user">' + userImg + username + '</div>' + playerCode + '</div>';
+  console.log(html);
+  $('#allsongs').prepend(html);
 }
 
 var lastOpenInfoWin = null;
