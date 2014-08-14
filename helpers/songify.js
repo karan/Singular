ab81473d36c77a04ca7f8bf722a1e00a4a90642d
Karan Goel <karan@goel.im>
2014-08-13 21:39:43 -0700

    add procfile

--- helpers/songify.js
+++ helpers/songify.js
@@ -14,8 +14,6 @@ module.exports.songify = function(tweet, callback) {
 
   var url = tweet.entities.urls[0].expanded_url.toLowerCase();
 
-  console.log(url);
-
   if ((url.indexOf("soundcloud.com") === -1) &&
       (url.indexOf("snd.sc") === -1) && 
       (url.indexOf("youtube.com") === -1) &&
